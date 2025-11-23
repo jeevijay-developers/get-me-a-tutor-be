6ca876fa91516efa98ebb7e0f050beb2cf425855
AmitRathore16 <2023kuec2041@iiitkota.ac.in>
2025-11-23 20:43:17 +0530

    added port for testing

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const start = async () => {
     console.log("MongoDB connected");
 
     const port = process.env.PORT || 5001;
-    app.listen(port, () => console.log(`Server running on port ${port}`));
+    app.listen(port,'0.0.0.0', () => console.log(`Server running on port ${port}`));
   } catch (err) {
     console.error("Failed to start server:", err);
   }
